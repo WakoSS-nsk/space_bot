01546bebc5d2666d01b75872584252edc4738141
Vadim Kozlov <wadimkoslow11@gmail.com>
2022-06-05 17:26:21 +0300

    test1

---
+++ spacebot.py
@@ -0,0 +1,73 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
+
+load_dotenv()
+
+secret_token = os.getenv('TOKEN')
+NASA_KEY = os.getenv('NASA_TOKEN')
+URL = f'https://api.nasa.gov/planetary/apod?api_key={NASA_KEY}&count=1'
+
+
+def get_new_image():
+    response = get_url()
+    print(response[0])
+    return response[0]
+
+
+def get_url():
+    try:
+        response = requests.get(URL)
+    except Exception as error:
+        print(error)
+        new_url = 'https://api.thedogapi.com/v1/images/search'
+        response = requests.get(new_url)
+    response = response.json()
+    print(response[0])
+    random_pic = response[0].get('hdurl')
+    description = response[0].get('explanation')
+    list_objects = [random_pic, description]
+    return list_objects
+
+
+def get_description():
+    response = get_url()
+    print(response[1])
+    return response[1]
+
+
+def new_pic(update, context):
+    chat = update.effective_chat
+    context.bot.send_photo(chat.id, get_new_image())
+    context.bot.send_message(chat.id, get_description())
+
+
+def wake_up(update, context):
+    chat = update.effective_chat
+    name = update.message.chat.first_name
+    button = ReplyKeyboardMarkup([['/planet_photo']], resize_keyboard=True)
+    get_url()
+
+    context.bot.send_message(
+        chat_id=chat.id,
+        text='Привет, {}. Насладись бесконечным космосом'.format(name),
+        reply_markup=button
+    )
+
+    context.bot.send_photo(chat.id, get_new_image())
+    context.bot.send_message(chat.id, get_description())
+
+
+def main():
+    updater = Updater(token=secret_token)
+    updater.dispatcher.add_handler(CommandHandler('start', wake_up))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, new_pic))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
